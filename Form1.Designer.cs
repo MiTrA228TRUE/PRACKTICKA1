fee362f7c1b899ad848b37dd0cf49015c0d42928
hp <hp@DESKTOP>
2023-06-28 20:58:07 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.ItemColumnSpacing = 100;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.MarkerColor = System.Drawing.Color.Red;
+            series1.MarkerSize = 6;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(743, 657);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.Text = "График";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(819, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(381, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ВЫЧИСЛИТЬ ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbX
+            // 
+            this.tbX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbX.Location = new System.Drawing.Point(819, 269);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(137, 29);
+            this.tbX.TabIndex = 2;
+            this.tbX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_number_start_KeyPress);
+            // 
+            // tbY
+            // 
+            this.tbY.AllowDrop = true;
+            this.tbY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbY.Location = new System.Drawing.Point(1063, 269);
+            this.tbY.Name = "tbY";
+            this.tbY.ReadOnly = true;
+            this.tbY.Size = new System.Drawing.Size(137, 29);
+            this.tbY.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(828, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ВВЕДИТЕ Х";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1059, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "РЕЗУЛЬТАТ Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1053, 558);
+            this.Name = "Form1";
+            this.Opacity = 0.99D;
+            this.Text = "Полином Лагранжа";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        // задаем в массивы x и y соответсвующие значения 
+        double[] x = { -2.0, -1.4, -0.8, -0.2, 0.4, 1.0, 1.6, 2.2, 2.8, 3.4, 4.0, 4.6, 5.2, 5.8, 6.4 };
+        double[] y = { 10.21, 9.35, 8.67, 6.12, 4.95, 3.64, 2.11, 1.05, -0.53, -1.89, -2.47, -3.28, -4.75, -6.04, -7.41 };
+        public Form1()
+        {
+            InitializeComponent();
+            // во время запуска программы на графике chart1 отмечает точки с координатами  из массивов x и y 
+            for (int i = 0; i < x.Length; i++)
+            {
+                chart1.Series[0].Points.Add(new DataPoint(x[i], y[i]));
+            }
+        }
+        // создаем событие для ввода данных tbX, позволяющее вводить в поле лишь цифры,знак минуса и запятую
+        private void tb_number_start_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number1 = e.KeyChar;
+            if (!Char.IsDigit(number1) && number1 != 8 && number1 != 45 && number1 != ',')
+                e.Handled = true;     
+        }
+        // метод полинома Лагранжа для вводимого пользователем xValue
+        public static double LagrangePolynomial(double[] x, double[] y, double xValue)
+        {
+            double result = 0.0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                double term = y[i];
+                for (int j = 0; j < y.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        // при условии i!=j вычисляем Li(xValue) = ∏(xValue - xj) / (xi - xj) для каждой точки 
+                        term *= (xValue - x[j]) / (x[i] - x[j]);
+                    }
+                }
+                //Вычисляем значения полинома Лагранжа в точке xValue по формуле: L(xValue) = ∑(yj * Li(xValue))
+                result += term;
+            }
+            // Возвращаем значение функции в точке xValue, полученное с помощью интерполяции
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // создаем еще одну проверку ввода xValue в поле tbX
+            // falsch истина если строка не пуста
+            bool falsch = !string.IsNullOrEmpty(tbX.Text);
+            // если falsch истина, проверяем позицию запятой, если на первой позиции возвращаем ложь
+            if (falsch == true)
+                if (tbX.Text[0] == ',')
+                    falsch = false;
+            // проходим по всем символам строки, начиная со второго символа
+            for (int i = 1; i < tbX.Text.Length; i++)
+            {
+                // если находим знак минус возвращаем ложь
+                if (tbX.Text[i] == '-')
+                   falsch = false;
+                // если находим запятую, перед которой не цифра, возвращаем ложб
+                if ((tbX.Text[i] == ',' && !char.IsDigit(tbX.Text[i-1])))
+                    falsch = false;
+            }
+            // если falsch истина, то выполняем код, иначе вывод ошибки в окне MessageBox
+            if (falsch == true)
+            {
+                // конвертируем в тип Double значение строки поля tbX
+                double xValue = Double.Parse(tbX.Text);
+                //в result присваеваем значение функции в точке xValue, полученное с помощью интерполяции
+                double result = LagrangePolynomial(x, y, xValue);
+                // очищаем поле tbY и передаем значение result
+                tbY.Clear();
+                tbY.Text +=
+                    String.Format("{0}" + Environment.NewLine, result);
+            }
+            else 
+            {
+                MessageBox.Show("ОШИБКА ВВОДА Х!",
+                               "ВНИМАНИЕ!",
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Asterisk,
+                               MessageBoxDefaultButton.Button1);
+            }
+        } 
+    }
+}
